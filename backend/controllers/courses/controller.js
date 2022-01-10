e1661709dc85233f80c52c2c8b047af69a71b133
Leonardo <leonardo@pache.eng.br>
2022-01-10 18:35:55 +0100

    add be course Create and Find

--- backend/controllers/courses/controller.js
+++ backend/controllers/courses/controller.js
@@ -8,23 +8,43 @@ const getAllCourses = async () => {
 	return courses
 }
 
-const getUserCourses = async userId => {
+const getCourse = async courseId => {
 
-	const courses = await Course.find()
-		.populate('instructors')
-		.populate('reviews')
-	return courses
-}
-
-const getTrendCourses = async () => {
-
-	return await Course.find()
+	return await Course.findById(courseId)
 		.populate('instructors')
 		.populate('reviews')
 }
+
+const createCourse = async ({
+	attributes,
+	price,
+	title,
+	description,
+	instructors,
+	cover_image,
+	cover_movie,
+	category,
+	sub_category,
+	content_sections
+}) => {
+	const course = new Course({
+		attributes,
+		price,
+		title,
+		description,
+		instructors,
+		cover_image,
+		cover_movie,
+		category,
+		sub_category,
+		content_sections
+	});
+	const responseCourse = await course.save();
+	return responseCourse;
+};
 
 module.exports = {
 	getAllCourses,
-	getUserCourses,
-	getTrendCourses
+	getCourse,
+	createCourse,
 };
---
+++ backend/models/entities/create-course-request.js
@@ -0,0 +1,168 @@
+const Ajv = require('ajv');
+const addFormats = require('ajv-formats');
+
+const courseRequestSchema = {
+	$id: 'createCourseRequest',
+	type: 'object',
+	properties: {
+		title: {
+			type: 'string'
+		},
+		description: {
+			type: 'string'
+		},
+		instructors: {
+			type: 'array',
+			items: {
+				type: 'string'
+			}
+		},
+		cover_image: {
+			type: 'string'
+		},
+		cover_movie: {
+			type: 'string'
+		},
+		attributes: {
+			type: 'object',
+			properties: {
+				video_content_length: {
+					type: 'number'
+				},
+				num_articles: {
+					type: 'number'
+				},
+				num_practice_tests: {
+					type: 'number'
+				},
+				has_lifetime_access: {
+					type: 'boolean'
+				},
+				has_assignments: {
+					type: 'boolean'
+				},
+				has_certificate: {
+					type: 'boolean'
+				}
+			}
+		},
+		category: {
+			type: 'array',
+			items: {
+				type: 'object',
+				properties: {
+					name: {
+						type: 'string'
+					}
+				},
+				required: [
+					'name'
+				]
+			}
+		},
+		sub_category: {
+			type: 'array',
+			items: {
+				type: 'object',
+				properties: {
+					name: {
+						type: 'string'
+					}
+				},
+				required: [
+					'name'
+				]
+			}
+		},
+		price: {
+			type: 'object',
+			properties: {
+				amount: {
+					type: 'string'
+				},
+				currency: {
+					type: 'string'
+				},
+				currency_symbol: {
+					type: 'string'
+				},
+				price_string: {
+					type: 'string'
+				},
+				is_free: {
+					type: 'boolean'
+				}
+			},
+			required: [
+				'amount',
+				'currency',
+				'currency_symbol'
+			]
+		},
+		content_sections: {
+			type: 'array',
+			items: {
+				type: 'object',
+				properties: {
+					title: {
+						type: 'string'
+					},
+					order: {
+						type: 'number'
+					},
+					items: {
+						type: 'array',
+						items: {
+							type: 'object',
+							properties: {
+								name: {
+									type: 'string'
+								},
+								video: {
+									type: 'string'
+								},
+								order: {
+									type: 'number'
+								}
+							},
+							required: [
+								'name',
+								'video',
+								'order'
+							]
+						}
+					}
+				},
+				required: [
+					'title',
+					'order',
+					'items'
+				]
+			}
+		}
+	},
+	required: [
+		'price',
+		'title',
+		'description',
+		'instructors',
+		'cover_image',
+		'category',
+		'sub_category',
+		'content_sections'],
+	additionalProperties: false,
+};
+
+function validateSchema(model) {
+	const ajv = new Ajv();
+	addFormats(ajv);
+	const validate = ajv.compile(courseRequestSchema);
+	const isValid = validate(model);
+	const { errors } = validate;
+	return {
+		isValid,
+		errors,
+	};
+}
+
+module.exports = validateSchema;
