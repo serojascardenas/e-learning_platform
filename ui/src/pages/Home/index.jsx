c56e49df7c71069a515d0863fc51437761660500
srojas <sonia.rojas@tabbank.com>
2022-01-10 00:05:29 -0500

    Merge main

--- ui/src/pages/Home/index.jsx
+++ ui/src/pages/Home/index.jsx
@@ -1,12 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import CourseList from "../../components/Cards/CourseList";
 
 const Home = () => {
-
-	return (
-		<>
-			Home Page
-		</>
-	);
+  return <CourseList></CourseList>; 
 };
 
 export default Home;
